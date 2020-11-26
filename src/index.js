a00947d1da383d3d9962e17b0fc42c580877e6bb
David Escalera <davidescalra@xpertai.com>
2020-11-26 18:15:12 +0100

    refactor(app): moving to function instead of class

--- src/index.js
+++ src/index.js
@@ -3,27 +3,31 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { fetchWeather } from './lib/open-weather-map';
- 
+
 const { REACT_APP_OPENWEATHERMAP_API_KEY } = process.env;
 
-class App extends React.Component {
-  state = {
+const App = () => {
+  const [state, setState] = React.useState({
     temperature: undefined,
-    city:undefined,
+    city: undefined,
     country: undefined,
     humidity: undefined,
     description: undefined,
     error: undefined,
-  };
+  });
 
-  getWeather = async (e) => {
+  const getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value || 'Madrid';
     const country = e.target.elements.country.value || 'es';
-    const api_call = await fetchWeather(city, country, REACT_APP_OPENWEATHERMAP_API_KEY);
+    const api_call = await fetchWeather(
+      city,
+      country,
+      REACT_APP_OPENWEATHERMAP_API_KEY
+    );
     const data = await api_call.json();
     if (city && country) {
-      this.setState({
+      setState({
         temperature: data.main.temp,
         city: data.name,
         country: data.sys.country,
@@ -32,7 +36,7 @@ class App extends React.Component {
         error: '',
       });
     } else {
-      this.setState({
+      setState({
         temperature: undefined,
         city: undefined,
         country: undefined,
@@ -43,80 +47,75 @@ class App extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <div>
-        <div className="wrapper">
-          <div className="main">
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-5 title-container">
-                  <div>
-                    <h1 className="title-container__title">Weather Finder</h1>
-                    <h3 className="title-container__subtitle">
-                      Find out temperature, conditions and more...
-                    </h3>
-                  </div>
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-5 title-container">
+                <div>
+                  <h1 className="title-container__title">Weather Finder</h1>
+                  <h3 className="title-container__subtitle">
+                    Find out temperature, conditions and more...
+                  </h3>
                 </div>
-                <div className="col-7 form-container">
-                  <form onSubmit={this.getWeather}>
-                    <input type="text" name="city" placeholder="Madrid" />
-                    <input type="text" name="country" placeholder="es" />
-                    <button>Get Weather</button>
-                  </form>
-                  <div className="weather__info">
-                    {this.state.city && this.state.country && (
-                      <p className="weather__key">
+              </div>
+              <div className="col-7 form-container">
+                <form onSubmit={getWeather}>
+                  <input type="text" name="city" placeholder="Madrid" />
+                  <input type="text" name="country" placeholder="es" />
+                  <button>Get Weather</button>
+                </form>
+                <div className="weather__info">
+                  {state.city && state.country && (
+                    <p className="weather__key">
+                      {' '}
+                      Location:
+                      <span className="weather__value">
                         {' '}
-                        Location:
-                        <span className="weather__value">
-                          {' '}
-                          {this.state.city}, {this.state.country}
-                        </span>
-                      </p>
-                    )}
-                    {this.state.temperature && (
-                      <p className="weather__key">
+                        {state.city}, {state.country}
+                      </span>
+                    </p>
+                  )}
+                  {state.temperature && (
+                    <p className="weather__key">
+                      {' '}
+                      Temperature:
+                      <span className="weather__value">
                         {' '}
-                        Temperature:
-                        <span className="weather__value">
-                          {' '}
-                          {this.state.temperature}{' '}
-                        </span>
-                      </p>
-                    )}
-                    {this.state.humidity && (
-                      <p className="weather__key">
+                        {state.temperature}{' '}
+                      </span>
+                    </p>
+                  )}
+                  {state.humidity && (
+                    <p className="weather__key">
+                      {' '}
+                      Humidity:
+                      <span className="weather__value"> {state.humidity} </span>
+                    </p>
+                  )}
+                  {state.description && (
+                    <p className="weather__key">
+                      {' '}
+                      Conditions:
+                      <span className="weather__value">
                         {' '}
-                        Humidity:
-                        <span className="weather__value">
-                          {' '}
-                          {this.state.humidity}{' '}
-                        </span>
-                      </p>
-                    )}
-                    {this.state.description && (
-                      <p className="weather__key">
-                        {' '}
-                        Conditions:
-                        <span className="weather__value">
-                          {' '}
-                          {this.state.description}{' '}
-                        </span>
-                      </p>
-                    )}
-                    {this.state.error && (
-                      <p className="weather__error">{this.state.error}</p>
-                    )}
-                  </div>
+                        {state.description}{' '}
+                      </span>
+                    </p>
+                  )}
+                  {state.error && (
+                    <p className="weather__error">{state.error}</p>
+                  )}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
