8982c05f1d3fd972a1d7b86b0b7b0cfd1bb772aa
David Escalera <davidescalra@xpertai.com>
2020-11-27 17:33:59 +0100

    feature(weather-form): saving last result to local storage

--- src/components/features/weather-form/index.js
+++ src/components/features/weather-form/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Form from './form';
 import FormResult from './form-result';
+import useLocalStorage from '../../hooks/use-local-storage';
 
 const WeatherForm = ({ onRequestWeather }) => {
+  const [cachedResult, setCachedResult] = useLocalStorage('result-cache', {});
   const [weatherData, setWeatherData] = React.useState({
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
+    temperature: cachedResult?.temperature,
+    city: cachedResult?.city,
+    country: cachedResult?.country,
+    humidity: cachedResult?.humidity,
+    description: cachedResult?.description,
     error: undefined,
   });
 
@@ -33,14 +35,16 @@ const WeatherForm = ({ onRequestWeather }) => {
       try {
         const data = await onRequestWeather(city, country);
 
-        setWeatherData({
+        const newState = {
           temperature: data.main.temp,
           city: data.name,
           country: data.sys.country,
           humidity: data.main.humidity,
           description: data.weather[0].description,
           error: undefined,
-        });
+        };
+        setWeatherData(newState);
+        setCachedResult(newState);
       } catch (e) {
         showError('Sorry, we did not find this city and/or country.');
       }
